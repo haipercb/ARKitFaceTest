609e76a272e6e0fec204d40675fe48c3b8c450a1
cuibo <zcsoft@icloud.com>
2023-02-09 18:36:43 +0800

    Initial Commit

---
+++ ARKitFaceTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ARKitFaceTest
+//
+//  Created by bo cui on 2023/2/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
